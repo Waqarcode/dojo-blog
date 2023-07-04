614153d0cf975d7ed0278b19665e20e1c1825593
Waqar Hussain <wikichaudhary5@gmail.com>
2023-07-04 15:43:03 +0500

    Click Event

--- src/Components/Home/HomeComponent.js
+++ src/Components/Home/HomeComponent.js
@@ -1,7 +1,23 @@
 const Home = () => {
+
+    const handleClickMe = (e) => {
+        console.log("Clicked, Fuction Call with Reference", e)
+    }
+
+    const handleClickMeAgain = (name, e) => {
+        console.log("Hi " + name, e.target)
+    }
+
     return ( 
-        <div className="home">Home Page</div>
+        <div className="home">
+            <h2>Home Page</h2>
+            {/* <button onClick={handleClickMe()}> Click Me</button> this will revoke the function on runtime withOut Click*/}
+            <button onClick={handleClickMe}> Click Me</button>
+            <button onClick={(e)=> handleClickMeAgain("Robot", e)}> Click Me Again</button>
+        </div>
      );
+
+
 }
  
 export default Home;
