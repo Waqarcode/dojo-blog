d92f6c966d99170421ff25514d67bbf955620631
Waqar Hussain <wikichaudhary5@gmail.com>
2023-07-05 21:57:42 +0500

    catch fetch Error

--- src/Components/Home/HomeComponent.js
+++ src/Components/Home/HomeComponent.js
@@ -7,6 +7,8 @@ const Home = () => {
 
   const [isLoading, setIsLoading] = useState(true);
 
+  const [error, setError] = useState(null);
+
   useEffect(() => {
     fetch('http://localhost:8000/blogs')
       .then(res => {
@@ -15,14 +17,22 @@ const Home = () => {
       .then(data => {
         console.log(data);
         setIsLoading(false);
+        setError(null)
         setBlogs(data);
-      });
-
+      })
+      .catch(err => {
+        //debugger;
+        setIsLoading(false);
+        setError(err.message);
+      })
   }, [])
+  
+
 
   return (
     <div className="home">
       {isLoading && <div>Loading...</div>}
+      { error && <div>{error}</div> }
       {blogs && <BlogList blogs={blogs} title={"All Blogs"} />}
     </div>
   );
