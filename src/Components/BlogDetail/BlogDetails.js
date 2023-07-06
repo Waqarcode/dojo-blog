6852ddf6fffdb158ae77441de6453ae9fb925e82
Waqar Hussain <wikichaudhary5@gmail.com>
2023-07-06 23:56:40 +0500

    Delete Request

--- src/Components/BlogDetail/BlogDetails.js
+++ src/Components/BlogDetail/BlogDetails.js
@@ -1,10 +1,22 @@
 import { useParams } from "react-router-dom";
 import useFetch from "../../EndPoint/useFetch";
+import { useHistory } from "react-router-dom";
+
 
 const BlogDetails = () => {
     const { id } = useParams();
     const {data: blog, isLoading, error} = useFetch('http://localhost:8000/blogs/' + id);
-    
+    const history = useHistory();
+
+    const handleClick = ()=>{
+        fetch('http://localhost:8000/blogs/' + blog.id,{
+            method: 'DELETE',
+        })
+        .then(()=>{
+            history.push('/');
+        })
+    }
+
     return (  
         <div className="blog-details">
             {isLoading && <div>Loading...</div> }
@@ -14,6 +26,7 @@ const BlogDetails = () => {
                     <h2>{blog.title}</h2>
                     <p>written by {blog.author}</p>
                     <div>{blog.body}</div>
+                    <button onClick={handleClick}>Delete</button>
                 </article>
             )}
         </div>
